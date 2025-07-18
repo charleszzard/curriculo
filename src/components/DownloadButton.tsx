65f031469f33839a240849f7b96bd388a10493fd
Charles Junio Gomes dos Santos <charles.it2b@trtes.jus.br>
2025-07-18 11:58:59 -0300

    Ajusta responsividade e corrige botão de download do currículo

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -2,7 +2,7 @@ export const DownloadButton = () => {
   return (
    <div className="flex justify-center mt-10">
       <a
-        href="../src/assets/Curriculo Charles Junio.pdf"
+        href="public/Curriculo Charles Junio.pdf"
         download
         className="w-full sm:w-auto text-center bg-blue-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700 transition"
       >
